968e002de5ebf77f7636e959e5aff42fae22b4d5
abdallaFoad <foadabdalla314@gmail.com>
2023-01-28 20:37:04 +0200

    add vercel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <title>Abdalla Foad | Front-end Developer</title>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
           rel="stylesheet"
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import { Service } from "../types";
 import { motion } from "framer-motion";
 import { fadeInUp, routeAnimate, stagger } from "../animations";
 
-const About: NextPage = () => {
+const About = ({ endpoint }:any)  => {
   return (
     <motion.div
       className="flex flex-col flex-grow px-6 pt-1"
@@ -54,5 +54,33 @@ const About: NextPage = () => {
     </motion.div>
   );
 };
+
+
+
+// !called every time  the page refreshed
+export const getServerSideProps: GetServerSideProps = async (
+   context: GetServerSidePropsContext
+) => {
+  //  const res = await fetch(`${process.env.VERCEL_URL}/api/services`)
+  //  const data = await res.json()
+  //  console.log(data)
+   return { props: { endpoint: process.env.VERCEL_URL } };
+}
+
+// !called only during the build of the project
+// ? make sure the server(localhost:3000)[this will receive the request during build] is running on a terminal during the build
+// ? also need to change the localhost during the deployment | see the todo
+// https://aude53.medium.com/set-environment-variables-with-next-js-and-vercel-e544c0460a48
+
+// export const getStaticProps: GetStaticProps = async (
+//    context: GetStaticPropsContext
+// ) => {
+//    // console.log(context);
+
+//    const res = await fetch('http://localhost:3000/api/services')
+//    const { services } = await res.json()
+//    console.log({ services })
+//    return { props: { services: services } }
+// }
 
 export default About;
